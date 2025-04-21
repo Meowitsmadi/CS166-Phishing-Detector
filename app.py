fc1778317c1aba230f3a730cbf2b6b56d5cf9e00
balanxiao <131484078+balanxiao@users.noreply.github.com>
2025-04-20 19:49:48 -0700

    Basic Interface
    Button, Input textbox completed

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+#.venv\Scripts\activate.bat
+# pip install streamlit
 import streamlit as st
 
-st.write("Hello Worlddsasd")
+st.title("Phishing Link Detector")
+
+# Text entry box
+user_input = st.text_input("Enter the link to check:")
+
+# Submit button
+if st.button("Submit"):
+    if user_input:
+        st.success(f"Processing link: {user_input}")
+        #Placee Holder value before we pass values to algorithim
+    else:
+        st.warning("Please enter a link before submitting.")
