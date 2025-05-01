86553ecb3a69f1a7e8a0358944177366ecaafd00
balanxiao <131484078+balanxiao@users.noreply.github.com>
2025-05-01 12:33:52 -0700

    Connection between frontend and database
    Front end can detect phishing links

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-#.venv\Scripts\activate.bat
-# pip install streamlit
 import streamlit as st
+from phishing_detector import phish_checker
 
 st.title("Phishing Link Detector")
 
@@ -10,7 +9,7 @@ user_input = st.text_input("Enter the link to check:")
 # Submit button
 if st.button("Submit"):
     if user_input:
-        st.success(f"Processing link: {user_input}")
-        #Placee Holder value before we pass values to algorithim
+        result = phish_checker(user_input)
+        st.success(f"Result: {result}")
     else:
         st.warning("Please enter a link before submitting.")
